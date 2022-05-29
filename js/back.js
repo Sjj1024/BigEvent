766dfc8e8bb79dd8cf135fe6086370a2dbfd9cb9
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2022-05-29 22:07:14 +0800

    后台中心区域绘制

--- js/back.js
+++ js/back.js
@@ -1,11 +1,9 @@
 $(function () {
     // 调整菜单互斥打开
     $(".layui-nav li>a").on("click", function (e) {
-        // console.log(this);
         $(this).parent().siblings().removeClass("layui-nav-itemed").removeClass("layui-this");
         if ($(this).parent().find("dl").length) {
-            // console.log($(this).parent().find("dl").children().eq(0).children());
-            $(this).parent().find("dl").children().eq(0).children().click();
+            $(this).parent().find("dl").children().eq(0).children()[0].click();
         }
     })
 })
